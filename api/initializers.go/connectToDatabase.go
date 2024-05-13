f3834f46878bfb7f989525394ad1c248e57efc26
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-05-13 16:48:59 +0100

    db connection

---
+++ api/initializers.go/connectToDatabase.go
@@ -0,0 +1,23 @@
+package initialzers
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDb() {
+	var err error
+	dsn := os.Getenv("DB")
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect with db")
+	}
+
+	fmt.Println("Successfully connected to the database")
+}
