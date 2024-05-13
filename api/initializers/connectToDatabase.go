5908925b71b9cb3967b3d5be69ea5d6b211d6e88
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-05-13 21:37:32 +0100

    db connection + testing

--- api/initializers/connectToDatabase.go
+++ api/initializers/connectToDatabase.go
@@ -2,6 +2,7 @@ package initialzers
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"gorm.io/driver/postgres"
@@ -10,14 +11,30 @@ import (
 
 var DB *gorm.DB
 
-func ConnectToDb() {
-	var err error
-	dsn := os.Getenv("DB")
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
-	if err != nil {
-		panic("Failed to connect with db")
+func ConnectToDB() {
+	// Load environment variables with default fallbacks
+	host := os.Getenv("POSTGRES_HOST")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DATABASE")
+	port := os.Getenv("POSTGRES_PORT")
+	if port == "" {
+		port = "5432" // Default PostgreSQL port
+	}
+	sslmode := os.Getenv("POSTGRES_SSLMODE")
+	if sslmode == "" {
+		sslmode = "require" // Default to "require" for production environments
 	}
 
+	// Construct the connection string
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
+		host, user, password, dbname, port, sslmode)
+
+	// Attempt to connect to the database
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
 	fmt.Println("Successfully connected to the database")
 }
