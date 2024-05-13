30d28abff0b16643189785bd80b571c6656ef579
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-05-13 17:46:11 +0100

    update

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -1,12 +1,9 @@
-package main
+package api
 
 import (
-	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 var (
@@ -25,26 +22,6 @@ func init() {
 	app = gin.New()
 	r := app.Group("/api")
 	iniRoute(r)
-}
-
-func main() {
-
-	println("Starting the application...")
-	app = gin.New()
-	r := app.Group("/api")
-	iniRoute(r)
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
-
-	if os.Getenv("ENV") == "development" {
-		println("Running in development mode on port 8080...")
-		app.Run(":8080")
-	} else {
-		println("ENV not set to 'development', application will not start the server.")
-	}
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
