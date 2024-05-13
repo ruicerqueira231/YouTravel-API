7e602767bbbdf3fb7fb1fee4deab84c228e4fb73
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-05-13 18:58:15 +0100

    online testing

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -24,7 +24,7 @@ func init() {
 	app = gin.New()
 	r := app.Group("/api")
 	iniRoute(r)
-	r.POST("/signup", controllers.Signup)
+	r.POST("/signup", controllers.SignupHandler())
 	r.POST("/login", controllers.Login)
 	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
 	r.POST("/logout", middleware.RequireAuth, controllers.Logout)
