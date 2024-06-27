c26d6369fe3299d19b4ce0d5aabd652650d969a1
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-06-27 21:53:37 +0100

    test upload image with remote api

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -1,9 +1,11 @@
 package api
 
 import (
-	"fmt"
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"net/http"
-	"os"
 	initialzers "youtravel-api/api/initializers"
 	"youtravel-api/api/routes"
 
@@ -14,14 +16,41 @@ var (
 	app *gin.Engine
 )
 
-func PrintAllEnvVariables() {
-	for _, env := range os.Environ() {
-		fmt.Println(env)
+func ImageUpload(c *gin.Context) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to load AWS config"})
+		return
 	}
+
+	client := s3.NewFromConfig(cfg)
+
+	file, header, err := c.Request.FormFile("fileUpload")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file retrieval error"})
+		return
+	}
+	defer file.Close()
+
+	bucket := "you-travel-storage"
+	key := header.Filename
+
+	_, err = client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   file,
+		ACL:    "public-read",
+	})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to upload to S3"})
+		return
+	}
+
+	// Success response
+	c.JSON(http.StatusOK, gin.H{"message": "Successfully uploaded file"})
 }
 
 func init() {
-	PrintAllEnvVariables()
 
 	initialzers.LoadEnvVariables()
 
@@ -39,6 +68,12 @@ func init() {
 	routes.InviteRoutes(r)
 	routes.CategoriesRoutes(r)
 	routes.LocationRoutes(r)
+
+	//image
+	r.GET("/image", func(c *gin.Context) {
+		c.File("index.html")
+	})
+	r.POST("/upload", ImageUpload)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
