a0990182fe074b789d6896d7f78dc0665f6e0b1f
Rui Cerqueira <ruicerqueira647@gmail.com>
2024-06-27 13:13:56 +0100

    update to log the error more speceficly

--- api/initializers/loadEnvVariables.go
+++ api/initializers/loadEnvVariables.go
@@ -8,11 +8,20 @@ import (
 )
 
 func LoadEnvVariables() {
-	log.Printf("Current ENV: %s", os.Getenv("ENV"))
-	if os.Getenv("ENV") != "production" {
+	env := os.Getenv("ENV")
+	if env == "" {
+		log.Printf("ENV variable not set, defaulting to .env file loading")
 		err := godotenv.Load()
 		if err != nil {
 			log.Fatalf("Error loading .env file: %v", err)
+		}
+	} else {
+		log.Printf("Current ENV: %s", env)
+		if env != "production" {
+			err := godotenv.Load()
+			if err != nil {
+				log.Fatalf("Error loading .env file: %v", err)
+			}
 		}
 	}
 }
